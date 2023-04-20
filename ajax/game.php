a8ee53dac2343c0570ff617aa01f8847c53760be
PythonSupportForum <114379208+PythonSupportForum@users.noreply.github.com>
2023-04-20 22:03:05 +0200

    Erste Spielbare Version!

---
+++ ajax/game.php
@@ -0,0 +1,205 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 30000');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+function generateRandomString($length = 10): string
+{
+    try {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    } catch(Exception $ex){
+        return "xyz";
+    }
+}
+
+function objectToArray($obj) {
+    $arr = is_object($obj) ? get_object_vars($obj) : $obj;
+    foreach ($arr as $key => $val) {
+        if (is_object($val) || is_array($val)) {
+            $arr[$key] = objectToArray($val);
+        } else {
+            $arr[$key] = $val;
+        }
+    }
+    return $arr;
+}
+
+function checkData($key): bool
+{
+    $data = getDataFromFile();
+    return isset($data[$key]);
+}
+function removeData($key){
+    $data = getDataFromFile();
+    unset($data[$key]);
+    saveDataToFile($data);
+}
+
+function getData($key){
+    $data = getDataFromFile();
+    return $data[$key] ?? null;
+}
+
+function setData($key, $value){
+    $data = getDataFromFile();
+    $data[$key] = $value;
+    saveDataToFile($data);
+}
+
+function getDataFromFile(){
+    $data = file_get_contents('data.json');
+    return json_decode($data, true);
+}
+
+function saveDataToFile($data){
+    $json_data = json_encode($data);
+    file_put_contents('data.json', $json_data);
+}
+const GAME = 0;
+
+$playerId = $_POST['id'] ?? "xyz";
+$action = $_POST['action'] ?? "update";
+$field = isset($_POST['my_field']) ? (array)json_decode($_POST['my_field']) : array();
+
+if(checkData("/tmp/".GAME."_players_".$playerId)){
+    $gameId = getData("/tmp/".GAME."_players_".$playerId);
+    $gameData = objectToArray(json_decode(getData("/tmp/".GAME."_games_".$gameId)));
+} else {
+    $ok = false;
+    if(checkData("/tmp/".GAME."_waiting_game")){
+        $gameId = getData("/tmp/".GAME."_waiting_game");
+        removeData("/tmp/".GAME."_waiting_game");
+        $gameData = objectToArray(json_decode(getData("/tmp/".GAME."_games_".$gameId)));
+        if(isset($gameData['a']['last']) && time() - $gameData['a']['last'] > 1){
+            removeData("/tmp/".GAME."_players_".$gameData['a']['id']);
+            removeData("/tmp/".GAME."_games_".$gameId);
+            unset($gameData);
+        } else {
+            $gameData['started'] = true;
+            $gameData['b'] = array(
+                "id" => $playerId,
+                "field" => $field
+            );
+            setData("/tmp/" . GAME . "_games_" . $gameId, json_encode($gameData));
+            $ok = true;
+        }
+    }
+    if(!$ok){
+        $gameId = $playerId.generateRandomString(8);
+        $gameData = array(
+            "started" => false,
+            "aPlayer" => $playerId,
+            "a" => array(
+                "id" => $playerId,
+                "field" => $field,
+                "last" => time()
+            ),
+            "b" => false,
+            "dran" => ((rand(0,1) == 1) ? "a" : "b")
+        );
+        setData("/tmp/".GAME."_games_".$gameId, json_encode($gameData));
+        setData("/tmp/".GAME."_waiting_game", $gameId);
+    }
+    if(isset($gameId)) setData("/tmp/".GAME."_players_".$playerId, $gameId);
+}
+
+$dran = ($gameData["aPlayer"] == $playerId ? "a" : "b") == $gameData["dran"];
+
+function check_won(): bool
+{
+    global $gameData, $playerId;
+    $fields = $gameData[($gameData["aPlayer"] == $playerId ? "b" : "a")]['field'];
+    foreach($fields as $row) {
+        foreach ($row as $f) {
+            if(!isset($f['status'])) $f['status'] = false;
+            if(!isset($f['beaten'])) $f['beaten'] = false;
+            if($f['status'] && !$f['beaten']) return false;
+        }
+    }
+    return true;
+}
+
+if($gameData['started'] && check_won()){
+    $gameData['winner'] = ($gameData["aPlayer"] == $playerId ? "a" : "b");
+    if(isset($gameId)) setData("/tmp/".GAME."_games_".$gameId, json_encode($gameData));
+}
+
+if(!$gameData['started']) {
+    $infoText = "Warten auf Gegner..";
+    $my_status = 1;
+    $gegner_status = 2;
+} else if(isset($gameData['winner'])){
+    if($gameData['winner'] == ($gameData["aPlayer"] == $playerId ? "a" : "b")){
+        $infoText = "Du hast Gewonnen!";
+        $my_status = 1;
+        $gegner_status = 5;
+    } else {
+        $infoText = "Du hast Verloren!";
+        $my_status = 6;
+        $gegner_status = 3;
+    }
+} else if($dran){
+    $infoText = "Du bist dran!";
+    $my_status = 1;
+    $gegner_status = 4;
+} else {
+    $infoText = "Dein Gegner ist dran!";
+    $my_status = 1;
+    $gegner_status = 3;
+}
+
+if($action == "hit" && $dran) {
+    $hitX = $_POST['hitX'];
+    $hitY = $_POST['hitY'];
+
+    $gameData[($gameData["aPlayer"] == $playerId ? "b" : "a")]['field'][$hitY][$hitX]['beaten'] = true;
+
+    if(!isset($gameData[($gameData["aPlayer"] == $playerId ? "b" : "a")]['field'][$hitY][$hitX]['status']) || !$gameData[($gameData["aPlayer"] == $playerId ? "b" : "a")]['field'][$hitY][$hitX]['status']){
+        $gameData["dran"] = ($gameData["aPlayer"] == $playerId ? "b" : "a");
+    }
+
+    if(isset($gameId)) setData("/tmp/".GAME."_games_".$gameId, json_encode($gameData));
+}
+
+function show_all_beaten_fields($field): array
+{
+    $newField = array();
+    foreach($field as $row){
+        $rowFields = array();
+        foreach($row as $f){
+            if(isset($f['status']) && $f['status']){
+                $f['status'] = true;
+            } else {
+                $f['status'] = false;
+            }
+            $rowFields[] = array(
+                'beaten' => $f['beaten'] ?? false,
+                'status' => ($f['beaten'] && $f['status'] ?? false)
+            );
+        }
+        $newField[] = $rowFields;
+    }
+    return $newField;
+}
+
+echo json_encode(array(
+    'id' => $playerId,
+    'info_message' => $infoText,
+    'my_status' => $my_status,
+    'gegner_status' => $gegner_status,
+    'my_field' => ((array)$gameData[($gameData["aPlayer"] == $playerId ? "a" : "b")])['field'],
+    'gegner_field' => $gameData['started'] ? show_all_beaten_fields(($gameData[($gameData["aPlayer"] == $playerId ? "b" : "a")])['field']) : array()
+));
+
+return exit();
--- js/script.js
+++ js/script.js
@@ -2,19 +2,15 @@ class GameField {
     constructor (status = 0, h = 10, w = 10, ships = {
         2: {
             name: "Ruderboot",
-            count: 4
+            count: 3
         },
         3: {
             name: "Motorschiff",
-            count: 3
-        },
-        4: {
-            name: "Frachtschiff",
             count: 2
         },
-        5: {
+        4: {
             name: "Kreuzer",
-            count: 1
+            count: 2
         }
     }){
         this.ship_types = ships;
@@ -38,7 +34,7 @@ class GameField {
         info_container.classList.add("footer_info");
         this.info_container = info_container;
 
-        function create_field(x, y){
+        function create_field(){
             let f = document.createElement("div");
             let img = document.createElement("img");
             img.alt = "Schiff Icon";
@@ -52,28 +48,23 @@ class GameField {
             row.classList.add("row");
             let row_fields = [];
             for(let x = 0; x < this.w; x++){
-                let element = create_field(x, y);
+                let element = create_field();
                 element.style.height = (350/this.h)+"px";
                 element.style.width = (350/this.w)+"px";
                 row.appendChild(element);
-                let field_data = {status: false, element: element, x: x, y: y, texture: false};
+                let field_data = {status: false, element: element, x: x, y: y, texture: false, beaten: false};
                 row_fields.push(field_data);
                 element.onclick = function(){
                     if(this.field.status === 0) {
                         if(!this.data.status) {
-                            if(this.field.field_status(this.field.fields, this.data.x-1, this.data.y-1) || this.field.field_status(this.field.fields, this.data.x+1, this.data.y+1) || this.field.field_status(this.field.fields, this.data.x-1, this.data.y+1) || this.field.field_status(this.field.fields, this.data.x+1, this.data.y-1)) return;
-                            let test = JSON.parse(JSON.stringify(this.field.fields));
-                            test[this.data.y][this.data.x] = {status: true};
-                            let length = this.field.ship_length(test, this.data.x, this.data.y);
-                            if(length > this.field.max_ship_length+1) return;
+                            if(!this.field.checkField(x, y)) return;
                         }
-                        this.data.status = !this.data.status;
-                        if (this.data.status) {
-                            this.data.element.classList.add("ship");
-                        } else {
-                            this.data.element.classList.remove("ship");
+                        this.field.setField(x, y, !this.data.status);
+                    } else if(this.field.status === 4) {
+                        if(!this.data.status) {
+                            this.field.setText("Wird verarbeitet...");
+                            Server.hit(this.data.x, this.data.y);
                         }
-                        this.field.update();
                     }
                 }.bind({field: this, data: field_data});
             }
@@ -87,7 +78,29 @@ class GameField {
         field.appendChild(info_container);
         document.getElementById("game_container").appendChild(field);
 
-        this.update();
+        this.update().then(() => {});
+
+        this.placeShipsRandomly().then(() => {});
+    }
+    checkField(x, y){
+        if(this.field_status(this.fields, x, y)) return false;
+
+        if(this.field_status(this.fields, x-1, y-1) || this.field_status(this.fields, x+1, y+1) || this.field_status(this.fields, x-1, y+1) || this.field_status(this.fields, x+1, y-1)) return;
+        let test = JSON.parse(JSON.stringify(this.fields));
+        test[y][x] = {status: true};
+        let length = this.ship_length(test, x, y);
+        return length <= this.max_ship_length + 1;
+    }
+    setField(x, y, status = true){
+        let fieldData = this.fields[y][x];
+        let oldStatus = fieldData.status;
+        fieldData.status = status;
+        if(!oldStatus && fieldData.status) {
+            fieldData.element.classList.add("ship");
+        } else if(oldStatus && !fieldData.status){
+            fieldData.element.classList.remove("ship");
+        }
+        this.update().then(() => {});
     }
     field_status(fields, x, y){
         if(x < 0 || x > 9 || y < 0 || y > 9) return false;
@@ -129,10 +142,62 @@ class GameField {
                 this.setText("Deine Schiffe:");
                 break;
             case 2:
+                this.setText("Warten auf Gegner..");
+                break;
+            case 3:
                 this.setText("Gegnerische Flotte:");
                 break;
+            case 4:
+                this.setText("Richte deine Kanonen aus!");
+                break;
+            case 5:
+                this.setText("Sieg über die Gegnerische Flotte!", "gold");
+                break;
+            case 6:
+                this.setText("Die Verlierer-Flotte", "red");
+                break;
         }
-        this.update();
+        this.update().then(() => {});
+    }
+    async placeShipsRandomly() {
+        while(await this.update()) {
+            for (let y = 0; y < this.h; y++) {
+                for (let x = 0; x < this.w; x++) {
+                    this.setField(x, y, false);
+                }
+            }
+            for (const ship in this.ship_types) {
+                let count = this.ship_types[ship].count;
+                while (count > 0) {
+                    let shipLength = parseInt(ship);
+                    let shipPlaced = false;
+                    while (!shipPlaced) {
+                        let orientation = Math.floor(Math.random() * 2); // 0 for horizontal, 1 for vertical
+                        let x = Math.floor(Math.random() * this.w);
+                        let y = Math.floor(Math.random() * this.h);
+                        let positions = [];
+                        let valid = true;
+                        for (let i = 0; i < shipLength; i++) {
+                            let posX = orientation ? x : x + i;
+                            let posY = orientation ? y + i : y;
+                            if (posX >= this.w || posY >= this.h || !this.checkField(posX, posY)) {
+                                valid = false;
+                                break;
+                            } else {
+                                positions.push({x: posX, y: posY});
+                            }
+                        }
+                        if (valid) {
+                            for (const position of positions) {
+                                this.setField(position.x, position.y, true);
+                            }
+                            shipPlaced = true;
+                        }
+                    }
+                    count--;
+                }
+            }
+        }
     }
     scan_ships(){
         let ships = {};
@@ -150,87 +215,184 @@ class GameField {
         return ships;
     }
     update(){
-        setTimeout(function(){
-            this.info_container.style.display = this.status === 0 ? "block" : "none";
-            if(this.status === 0) {
-                let html = "<ul>";
-                let ship_counts = this.scan_ships();
-                Object.keys(this.ship_types).forEach(function(length){
-                    html += '<li class="'+(ship_counts[length] !== this.ship_types[length].count ? "needed_ship" : "")+'">'+this.ship_types[length].name+' ('+length+' Kästchen): '+ship_counts[length]+'/'+this.ship_types[length].count+'</li>';
-                }.bind(this));
-                html += "</ul>";
-                let error = false;
-                let invalid_count = false;
-                let invalid_types = [];
-                Object.keys(ship_counts).forEach(function(length){
-                    if(!(length in this.ship_types)) {
-                        invalid_types.push(length);
-                        return error = true;
-                    }
-                    if(ship_counts[length] !== this.ship_types[length].count) {
-                        error = true;
-                        invalid_count = true;
-                    }
-                }.bind(this));
-                Object.keys(this.ship_types).forEach(function(length){
-                    if(ship_counts[length] !== this.ship_types[length].count) {
-                        error = true;
-                        invalid_count = true;
-                    }
-                }.bind(this));
-                if(!error) {
-                    html += '<button onclick="start_game();">🏁 Spiel Starten 🏁</button>';
-                } else {
-                    if(invalid_types.length > 0 && !invalid_count){
-                        html += '<div style="color: red; text-align: left; ">Folgende Schiffe werden in diesem Spiel Modi nicht unterstützt: <br><ul>';
-                        invalid_types.forEach(function(length){
-                            html += '<li>'+length+'er Boote</li>';
-                        });
-                        html += '</ul></div>';
-                    }
-                }
-                this.info_container.innerHTML = html;
-            }
-            for(let y = 0; y < this.h; y++) {
-                for (let x = 0; x < this.w; x++) {
-                    if (this.field_status(this.fields, x, y)) {
-                        let teil = "one";
-                        let rotation = false;
-                        if(this.field_status(this.fields, x, y+1) && this.field_status(this.fields, x, y-1)){
-                            rotation = true;
-                            teil = "middle";
+        return new Promise(function(resolve){
+            setTimeout(function(){
+                let PlacingError = false;
+                this.info_container.style.display = this.status === 0 ? "block" : "none";
+                if(this.status === 0) {
+                    let html = "<ul>";
+                    let ship_counts = this.scan_ships();
+                    Object.keys(this.ship_types).forEach(function(length){
+                        html += '<li class="'+(ship_counts[length] !== this.ship_types[length].count ? "needed_ship" : "")+'">'+this.ship_types[length].name+' ('+length+' Kästchen): '+ship_counts[length]+'/'+this.ship_types[length].count+'</li>';
+                    }.bind(this));
+                    html += "</ul>";
+                    let error = false;
+                    let invalid_count = false;
+                    let invalid_types = [];
+                    Object.keys(ship_counts).forEach(function(length){
+                        if(!(length in this.ship_types)) {
+                            invalid_types.push(length);
+                            return error = true;
                         }
-                        if(this.field_status(this.fields, x+1, y) && this.field_status(this.fields, x-1, y)){
-                            rotation = false;
-                            teil = "middle";
+                        if(ship_counts[length] !== this.ship_types[length].count) {
+                            error = true;
+                            invalid_count = true;
                         }
-                        if(this.field_status(this.fields, x+1, y) && !this.field_status(this.fields, x-1, y)){
-                            rotation = false;
-                            teil = "back";
+                    }.bind(this));
+                    Object.keys(this.ship_types).forEach(function(length){
+                        if(ship_counts[length] !== this.ship_types[length].count) {
+                            error = true;
+                            invalid_count = true;
                         }
-                        if(this.field_status(this.fields, x-1, y) && !this.field_status(this.fields, x+1, y)){
-                            rotation = false;
-                            teil = "front";
-                        }
-                        if(this.field_status(this.fields, x, y+1) && !this.field_status(this.fields, x, y-1)){
-                            rotation = true;
-                            teil = "back";
-                        }
-                        if(this.field_status(this.fields, x, y-1) && !this.field_status(this.fields, x, y+1)){
-                            rotation = true;
-                            teil = "front";
-                        }
-                        this.fields[y][x].element.children[0].style.display = "block";
-                        this.fields[y][x].element.children[0].src = "images/gameAssets/"+teil+(this.fields[y][x].status === true ? "" : "_dest")+".png";
-                        this.fields[y][x].element.children[0].style.transform = "rotate("+(rotation ? 90 : 0)+"deg)";
+                    }.bind(this));
+                    if(!error) {
+                        html += '<button onclick="start_game();">🏁 Spiel Starten 🏁</button>';
                     } else {
-                        this.fields[y][x].element.children[0].style.display = "none";
+                        PlacingError = true;
+                        if(invalid_types.length > 0 && !invalid_count){
+                            html += '<div style="color: red; text-align: left; ">Folgende Schiffe werden in diesem Spiel Modi nicht unterstützt: <br><ul>';
+                            invalid_types.forEach(function(length){
+                                html += '<li>'+length+'er Boote</li>';
+                            });
+                            html += '</ul></div>';
+                        }
+                    }
+                    this.info_container.innerHTML = html;
+                }
+                for(let y = 0; y < this.h; y++) {
+                    for (let x = 0; x < this.w; x++) {
+                        if (this.field_status(this.fields, x, y)) {
+                            let teil = "one";
+                            let rotation = false;
+                            if(this.field_status(this.fields, x, y+1) && this.field_status(this.fields, x, y-1)){
+                                rotation = true;
+                                teil = "middle";
+                            }
+                            if(this.field_status(this.fields, x+1, y) && this.field_status(this.fields, x-1, y)){
+                                rotation = false;
+                                teil = "middle";
+                            }
+                            if(this.field_status(this.fields, x+1, y) && !this.field_status(this.fields, x-1, y)){
+                                rotation = false;
+                                teil = "back";
+                            }
+                            if(this.field_status(this.fields, x-1, y) && !this.field_status(this.fields, x+1, y)){
+                                rotation = false;
+                                teil = "front";
+                            }
+                            if(this.field_status(this.fields, x, y+1) && !this.field_status(this.fields, x, y-1)){
+                                rotation = true;
+                                teil = "back";
+                            }
+                            if(this.field_status(this.fields, x, y-1) && !this.field_status(this.fields, x, y+1)){
+                                rotation = true;
+                                teil = "front";
+                            }
+                            this.fields[y][x].element.children[0].style.display = "block";
+                            let new_src = "images/gameAssets/"+teil+(this.fields[y][x].beaten ? "_dest" : "")+".png";
+                            if(new_src !== this.fields[y][x].element.children[0].src) this.fields[y][x].element.children[0].src = new_src;
+                            this.fields[y][x].element.children[0].style.transform = "rotate("+(rotation ? 90 : 0)+"deg)";
+                        } else {
+                            this.fields[y][x].element.children[0].style.display = "none";
+                        }
                     }
                 }
+                resolve(PlacingError);
+            }.bind(this), 20);
+        }.bind(this));
+    }
+    update_field(data){
+        for(let y = 0; y < data.length; y++) {
+            for (let x = 0; x < data[y].length; x++) {
+                if(!(y in this.fields)) continue;
+                if(!(x in this.fields[y])) continue;
+
+                if(this.fields[y][x].beaten && !data[y][x].beaten) {
+                    this.fields[y][x].element.classList.remove("beaten");
+                } else if(!this.fields[y][x].beaten && data[y][x].beaten){
+                    this.fields[y][x].element.classList.add("beaten");
+                }
+
+                this.fields[y][x].status = data[y][x].status;
+                this.fields[y][x].beaten = data[y][x].beaten;
             }
-        }.bind(this), 20);
+        }
+        this.update().then(() => {});
+    }
+    export(){
+        let data = [];
+        for(let y = 0; y < this.h; y++){
+            let row_fields = [];
+            for (let x = 0; x < this.w; x++) {
+                row_fields.push({status: this.fields[y][x].status, beaten: this.fields[y][x].beaten});
+            }
+            data.push(row_fields);
+        }
+        return data;
     }
 }
+class GegnerServer {
+    constructor() {
+        this.id = Math.random().toString()+Math.random().toString()+Math.random().toString();
+        this.update();
+        this.next_hit = false;
+    }
+    request(data) {
+        const formData = new FormData();
+        Object.keys(data).forEach(key => formData.append(key, data[key]));
+
+        return new Promise(function(resolve, reject) {
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://sinkships.com/ajax/game.php');
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject(Error('Fehler beim Laden der Daten. Statuscode: ' + xhr.status));
+                }
+            };
+            xhr.onerror = function() {
+                reject(Error('Es ist ein Netzwerkfehler aufgetreten.'));
+            };
+            xhr.send(formData);
+        });
+    }
+    update(){
+        let hit = this.next_hit;
+        this.next_hit = false;
+        this.request({id: this.id, action: hit ? "hit" : "update", my_field: JSON.stringify(my_field.export()), hitX: hit ? hit.x : false, hitY: hit ? hit.y : false}).then(function(data){
+            console.log(data);
+
+            if(data.id !== this.id) {
+                console.error("Invalid Player ID!");
+                document.getElementById("game_info_text").innerText = "Verbindungsfehler!";
+            } else {
+                document.getElementById("game_info_text").innerText = data.info_message;
+
+                my_field.setStatus(data.my_status);
+                gegner_field.setStatus(data.gegner_status);
+
+                my_field.update_field(data.my_field);
+                gegner_field.update_field(data.gegner_field);
+            }
+            setTimeout(this.update.bind(this), 50);
+        }.bind(this));
+    }
+    hit(x, y){
+        this.next_hit = {x: x, y: y};
+    }
+}
+
+window.start_game = function(){
+    my_field.setStatus(1);
+
+    window.gegner_field = new GameField(2);
+
+    document.getElementById("game_info_text").innerText = "Verbinden mit Server..";
+
+    window.Server = new GegnerServer();
+};
+
 window.onload = function(){
     window.my_field = new GameField(0);
 }
